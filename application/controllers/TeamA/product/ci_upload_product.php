9663f21078d6a852a229beddc0d468632360a812
gauravkad8 <kad.gaurav.gaurav447@gmail.com>
2019-01-20 13:53:47 +0530

    test

--- application/controllers/TeamA/product/ci_upload_product.php
+++ application/controllers/TeamA/product/ci_upload_product.php
@@ -28,14 +28,76 @@ class ci_upload_product extends CI_Controller {
 
     public function index()
     {
-        $this->load->view('TeamA/dashboard/admin/header');
-        $this->load->view('TeamA/product/upload_product');
-        $this->load->view('TeamA/dashboard/admin/footer');
+        $this->load_panel_data();
     }
 
+   public function upload_stl_file(){
+       $this->load->model('TeamA/product/product_model','up');
+
+            $rand = rand(55555,444444);
+       if(!is_file( getcwd().'/uploads/user/'.$this->session->dir_url."/product/".$_POST['pname'])){
+           mkdir( getcwd().'/uploads/user/'.$this->session->dir_url."/product/".$_POST['pname'], 0777, true);
+       }
+
+       if(!is_file( getcwd().'/uploads/user/'.$this->session->dir_url."/product/stl")){
+           mkdir( getcwd().'/uploads/user/'.$this->session->dir_url."/product/".$_POST['pname']."/stl", 0777, true);
+       }
+       if(!is_file( getcwd().'/uploads/user/'.$this->session->dir_url."/product/view")){
+           mkdir( getcwd().'/uploads/user/'.$this->session->dir_url."/product/".$_POST['pname']."/view", 0777, true);
+       }
+
+       $config['upload_path']   = getcwd().'/uploads/user/'.$this->session->dir_url."/product/".$_POST['pname']."/stl/";
+       $config['allowed_types'] = 'pdf|zip';
+       $config['max_size']      = 200000000;
+       //$config['max_width']     = 1024;
+       ///$config['max_height']    = 768;
+       $this->load->library('upload', $config);
+
+       if ( ! $this->upload->do_upload('userfile')) {
+           $error = array('error' => $this->upload->display_errors());
+           //$this->load->view('upload_passage', $error);
+           print_r($error);
+       }
+
+       else {
+           $data = array('upload_data' => $this->upload->data());
+           $test=array(
+               'file'=>$this->upload->data('file_name'),
+               'pname'=>$_POST['pname'],
+               'flag'=>0,
+               'userid'=>$this->session->user_id
+           );
+         //  $this->load->model('upload_product');
+           $this->db->insert('tblproduct',$test);
+           $this->load_panel_data();
+           echo "Success";
+           //$this->load->view('upload_success', $data);
+
+       }
 
 
+   }
 
+    public function load_panel_data(){
+        $user=$this->session->userdata('user');
+        $query = $this->db->get_where('tblproduct', array('userId' =>$this->session->user_id,'flag'=>'0'));
+        $data['data'] = $query->result();
+        $this->load->view('teamA/dashboard/admin/header');
+        $this->load->view('teamA/product/upload_product',$data);
+        $this->load->view('teamA/dashboard/admin/footer');
+
+    }
+
+     public function remove_stl_file(){
+         $data = array('flag' => '1');
+
+         $where = "userId =".$this->session->user_id." and id=".$_POST['product_id'];
+
+         $str = $this->db->update_string('tblproduct', $data, $where);
+         $this->db->query($str);
+         //echo $str;
+         $this->load_panel_data();
+     }
 }
 
 ?>
--- application/views/TeamA/product/upload_product.php
+++ application/views/TeamA/product/upload_product.php
@@ -3,22 +3,22 @@
          <div class="row">
              <div class="col-lg-12">
                  <div class="col-lg-6">
-                       <h3>Upload Product</h3>
-                       <form >
+                       <h3>Upload Product - <?php echo $this->session->dir_url;?></h3>
+                       <form method="post" action="<?php echo base_url()."index.php/teama/product/ci_upload_product/upload_stl_file/"?>" enctype="multipart/form-data">
                            <div class="form-group">
                                <label for="exampleInputEmail1">Product Name</label>
-                               <input type="email" class="form-control" id="email_address" aria-describedby="emailHelp" placeholder="Enter Product Name">
+                               <input type="text" class="form-control" name="pname"id="email_address" aria-describedby="emailHelp" placeholder="Enter Product Name">
                                <small id="emailHelp" class="form-text text-muted">Product Name Must Be Unique.</small>
                            </div>
 
                            <div class="form-group">
                                <label for="exampleInputEmail1">Product File</label>
-                               <input type="file" class="form-control" id="email_address" aria-describedby="emailHelp">
+                               <input type="file" class="form-control" name="userfile" id="email_address" aria-describedby="emailHelp">
                                <small id="emailHelp" class="form-text text-muted">Allowed File Type are STL,ZIP.</small>
                            </div>
 
                            <div class="form-group pull-right">
-                               <input type="button" class="btn btn-primary" id="product_send" value="Upload">
+                               <input type="submit" class="btn btn-primary" id="product_send" value="Upload">
                            </div>
 
                        </form>
@@ -40,19 +40,24 @@
                              </tr>
                              </thead>
                              <tbody>
-                             <tr>
-                                 <td>16300</td>
-                                 <td>Vehicle Body</td>
-                                 <td class="text-primary">160018</td>
-                                 <td><button type="button" class="btn btn-danger">Remove</button></td>
-                             </tr>
-                             <tr>
-                                 <td>16300</td>
-                                 <td>Vehicle Body</td>
-                                 <td class="text-primary">160018</td>
-                                 <td><button type="button" class="btn btn-danger">Remove</button></td>
-                             </tr>
 
+                             <?php foreach ($data as $d ){?>
+                            <form method="post" action='<?php echo base_url()."index.php/teama/product/ci_upload_product/remove_stl_file";?>'>
+                                 <tr>
+                                     <input type="hidden" name="product_id" value='<?php echo $d->id;?>'>
+                                     <td><?php
+                                         echo md5($d->id);
+                                         ?></td>
+                                     </td>
+                                     <td><?php echo $d->pname;?></td>
+                                     <td><?php echo $d->file;?></td>
+                                     <td><button type="submit" class="bnt bnt-primary">delete</button></td>
+
+                                 </tr>
+                            </form>
+                             <?php } ?>
+
+g
 
                              </tbody>
                          </table>
